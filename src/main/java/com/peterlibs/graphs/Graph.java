7f7e6dfee9e5e1c106394fa06195f611223980a6
pwalker91 <peterjwalker42@gmail.com>
2021-04-23 18:34:22 -0700

    removing some notes
    I had originally added some notes based on the wikipedia article on Graphs. It was useful to keep context of what a Graph should be able to do, but is no longer necessary as I have implemented (in some way) most of the functions identified

--- src/main/java/com/peterlibs/graphs/Graph.java
+++ src/main/java/com/peterlibs/graphs/Graph.java
@@ -280,19 +280,4 @@ public class Graph implements Serializable {
         return null;
     }
 
-    /*
-    What wikipedia says a graph aught to be able to tell about itself. Let's see if
-    I can implement all of these!
-        adjacent(G, x, y): tests whether there is an edge from the vertex x to the vertex y;
-        neighbors(G, x): lists all vertices y such that there is an edge from the vertex x to the vertex y;
-        add_vertex(G, x): adds the vertex x, if it is not there;
-        remove_vertex(G, x): removes the vertex x, if it is there;
-        add_edge(G, x, y): adds the edge from the vertex x to the vertex y, if it is not there;
-        remove_edge(G, x, y): removes the edge from the vertex x to the vertex y, if it is there;
-        get_vertex_value(G, x): returns the value associated with the vertex x;
-        set_vertex_value(G, x, v): sets the value associated with the vertex x to v.
-        get_edge_value(G, x, y): returns the value associated with the edge (x, y);
-        set_edge_value(G, x, y, v): sets the value associated with the edge (x, y) to v.
-     */
-
 }
